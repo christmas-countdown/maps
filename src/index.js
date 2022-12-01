dd4a0b58438c8e184bb8236e12f0acd642458d32
Isaac <git@eartharoid.me>
2022-12-01 16:38:40 +0000

    Use coordinates instead of index
    because the bot modifies the array

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import locations from './locations.json';
 
 const router = Router();
 
-router.get('/map-tile/:index.png', async (req, env, ctx) => {
-	const location = locations[req.params.index];
-	if (!location) return new Response('400', { status: 400 });
-	const { latitude, longitude } = location;
+router.get('/map-tile/:latitude,:longitude', async (req, env, ctx) => {
+	const { latitude, longitude } = req.params;
+	if (!latitude || !longitude) return new Response('400', { status: 400 });
 	const style = 'eartharoid/clb57t9dz001s14nv72iwn3n4';
 	const url = `https://api.mapbox.com/styles/v1/${style}/static/pin-l+ffffff(${longitude},${latitude})/${longitude},${latitude},6,0/600x400?access_token=${env.MAPBOX_TOKEN}`;
 	const res = await fetch(url, { cf: { cacheTtl: 86400 } });
